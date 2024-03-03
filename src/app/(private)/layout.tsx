b83aa063d88a7e89ccbcb596daf604dd8a41ce8d
ZLoganZ <ln26805@gmail.com>
2024-03-03 18:51:16 +0700

    Update package-lock.json and package.json

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,10 +1,16 @@
-import SideBar from '@/components/SideBar/Sidebar';
+import SideBar from "@/components/SideBar/Sidebar";
 
-export default function PrivateLayout({ children }: { children: React.ReactNode }) {
+export default function PrivateLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className='w-full overflow-hidden sm:flex'>
+    <div className="w-full overflow-hidden sm:flex">
       <SideBar />
-      <section className='flex h-[calc(100%-160px)] flex-1 overflow-auto md:h-full'>{children}</section>
+      <section className="flex h-[calc(100%-160px)] flex-1 overflow-auto md:h-full">
+        {children}
+      </section>
     </div>
   );
 }
