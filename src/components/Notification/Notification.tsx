992ab8da08f9ff9cc52c89081b8e210bc23c496a
AdminJohnk <truongtantai07042002@gmail.com>
2024-03-06 23:25:41 +0700

    Search Notification

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,168 @@
+'use client';
+
+import * as React from 'react';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { IoIosCheckmarkCircleOutline } from 'react-icons/io';
+import { Avatar, Divider } from '@mui/material';
+
+const NewNotification = [
+  {
+    user: {
+      name: 'John Michael',
+      avatar: '/assets/images/avatars/avatar-1.jpg'
+    },
+    content: 'who you might know, is on Instello',
+    time: '1 hour ago'
+  },
+  {
+    user: {
+      name: 'Alexa Gray',
+      avatar: '/assets/images/avatars/avatar-2.jpg'
+    },
+    content: 'started following you. Welcome him to your profile. 👋',
+    time: '4 hour ago'
+  },
+  {
+    user: {
+      name: 'Sarah Lee',
+      avatar: '/assets/images/avatars/avatar-3.jpg'
+    },
+    content: 'liked your post "Amazing sunset!"',
+    time: '8 hour ago'
+  }
+];
+
+const ThisWeekNotification = [
+  {
+    user: {
+      name: 'David Kim',
+      avatar: '/assets/images/avatars/avatar-4.jpg'
+    },
+    content: 'commented on your photo "Happy birthday!" 💖',
+    time: '12 hour ago'
+  },
+  {
+    user: {
+      name: 'Instello Team',
+      avatar: '/assets/images/logo-icon.png'
+    },
+    content: 'suggested some groups you might like to join.',
+    time: '1 day ago'
+  },
+  {
+    user: {
+      name: 'Emily Jones',
+      avatar: '/assets/images/avatars/avatar-5.jpg'
+    },
+    content: 'mentioned you in a post: "Check out this article by @username"',
+    time: '2 days ago'
+  },
+  {
+    user: {
+      name: 'William Chen',
+      avatar: '/assets/images/avatars/avatar-6.jpg'
+    },
+    content: 'is celebrating their work anniversary today! 😍  ',
+    time: '3 days ago'
+  },
+  {
+    user: {
+      name: 'Christine Lee',
+      avatar: '/assets/images/avatars/avatar-7.jpg'
+    },
+    content: 'started a new conversation with you. Say hi! ',
+    time: '4 days ago'
+  },
+  {
+    user: {
+      name: 'Instello Polls',
+      avatar: '/assets/images/logo-icon.png'
+    },
+    content: 'created a new poll: "What is your favorite food?" Vote now!',
+    time: '1 week ago'
+  },
+  {
+    user: {
+      name: 'Michael Brown',
+      avatar: '/assets/images/avatars/avatar-5.jpg'
+    },
+    content: 'shared a link: "Interesting article about social media trends"',
+    is_followed: false,
+    time: '2 weeks ago'
+  }
+];
+
+export interface INotificationProps {}
+
+export default function Notification(props: INotificationProps) {
+  return (
+    <div className='notification'>
+      <div className='title flex-between px-5 mb-6'>
+        <span className='h3-semibold'>Notification</span>
+        <div className='flex-start gap-2 *:size-6'>
+          <IoSettingsOutline />
+          <IoIosCheckmarkCircleOutline />
+        </div>
+      </div>
+      <div className='new'>
+        <div className='mx-5 base-bold mb-4'>New</div>
+        <div className='notification-list px-2'>
+          {NewNotification.map((item, index) => (
+            <div
+              key={index}
+              className='notification-item flex-between gap-3 px-3 py-2 mb-1 hover:bg-hover-1 hover:cursor-pointer rounded-lg duration-300'
+            >
+              <div className='left flex-start'>
+                <div className='avatar me-3'>
+                  <Avatar src={item.user.avatar} className='size-12' />
+                </div>
+                <div className='content'>
+                  <div>
+                    <span className='base-bold me-2'>{item.user.name}</span>
+                    <span>{item.content}</span>
+                  </div>
+                  <div className='text-text-2 small-regular mt-1'>
+                    {item.time}
+                  </div>
+                </div>
+              </div>
+              {/* <div className='right'>
+                {!item.is_followed && <div className='followed'>Follow</div>}
+              </div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Divider className='bg-border-1 my-5' />
+      <div className='this-week'>
+        <div className='mx-5 base-bold my-4'>This Week</div>
+        <div className='notification-list'>
+          {ThisWeekNotification.map((item, index) => (
+            <div
+              key={index}
+              className='notification-item flex-between gap-3 px-3 py-2 mb-1 hover:bg-hover-1 hover:cursor-pointer rounded-lg duration-300'
+            >
+              <div className='left flex-start'>
+                <div className='avatar me-3'>
+                  <Avatar src={item.user.avatar} className='size-12' />
+                </div>
+                <div className='content'>
+                  <div>
+                    <span className='base-bold me-2'>{item.user.name}</span>
+                    <span>{item.content}</span>
+                  </div>
+                  <div className='text-text-2 small-regular mt-1'>
+                    {item.time}
+                  </div>
+                </div>
+              </div>
+              {/* <div className='right'>
+                {!item.is_followed && <div className='followed'>Follow</div>}
+              </div> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -6,175 +6,263 @@ import { IoSearch } from 'react-icons/io5';
 import { MdOutlineExplore, MdArrowForwardIos } from 'react-icons/md';
 import { TbMessage } from 'react-icons/tb';
 import { BsCameraReels } from 'react-icons/bs';
-import { FiShoppingCart } from 'react-icons/fi';
 import { useSession } from 'next-auth/react';
 import { CiCirclePlus } from 'react-icons/ci';
-import { CgComponents, CgProfile } from 'react-icons/cg';
+import { CgProfile } from 'react-icons/cg';
 import { Avatar, Skeleton } from '@mui/material';
 import Link from 'next/link';
 
 import {
   HoverCard,
   HoverCardContent,
-  HoverCardTrigger,
+  HoverCardTrigger
 } from '@/components/ui/hover-card';
 import AccountDetail from '@/components/Profile/AccountDetail';
 import getImageURL, { cn } from '@/lib/utils';
 import Image from 'next/image';
+import Search from '../Search/Search';
+import Notification from '../Notification/Notification';
 
 export interface ISideBarProps {}
 
 export default function SideBar(props: ISideBarProps) {
-  const [open, setOpen] = React.useState(false);
-  const [collapse, setCollapse] = React.useState(true);
+  const [open, setOpen] = React.useState(true);
+
+  const [collapse, setCollapse] = React.useState(false);
+  const [openSearch, setOpenSearch] = React.useState(false);
+  const [openNotification, setOpenNotification] = React.useState(false);
+
+  const CloseAllComponent = () => {
+    setOpenSearch(false);
+    setOpenNotification(false);
+  };
 
   const MenuItem = [
     {
       title: 'Home',
       icon: <IoMdHome />,
-      link: '/',
+      link: '/'
     },
     {
       title: 'Search',
       icon: <IoSearch />,
       link: '',
       onclick: () => {
-        alert('Search');
-      },
+        if (openSearch) {
+          setCollapse(false);
+          setOpenSearch(false);
+        } else {
+          CloseAllComponent();
+          setCollapse(true);
+          setOpenSearch(true);
+        }
+      }
     },
     {
       title: 'Explore',
       icon: <MdOutlineExplore />,
-      link: '/explore',
+      link: '/explore'
     },
     {
       title: 'Messages',
       icon: <TbMessage />,
-      link: '/messages',
+      link: '/messages'
     },
     {
       title: 'Reels',
       icon: <BsCameraReels />,
-      link: '/reels',
+      link: '/reels'
     },
     {
       title: 'Notifications',
       icon: <IoMdNotificationsOutline />,
-      link: '/notifications',
+      link: '',
+      onclick: () => {
+        if (openNotification) {
+          setCollapse(false);
+          setOpenNotification(false);
+        } else {
+          CloseAllComponent();
+          setCollapse(true);
+          setOpenNotification(true);
+        }
+      }
     },
     {
       title: 'People',
       icon: <IoMdPeople />,
-      link: '/people',
+      link: '/people'
     },
     {
       title: 'Create',
       icon: <CiCirclePlus />,
-      link: '/create',
+      link: '/create'
     },
     {
       title: 'Profile',
       icon: <CgProfile />,
-      link: '/profile',
-    },
+      link: '/profile'
+    }
   ];
 
   const { data: session } = useSession();
 
   return (
     <nav
-      className={cn(
-        'side-bar fixed left-0 top-0 h-dvh bg-foreground-1 px-2 w-60 max-lg/2:w-20 overflow-y-scroll custom-scrollbar-none',
-      )}
+      className={cn('side-bar fixed left-0 top-0 h-dvh bg-foreground-1')}
+      style={{
+        zIndex: 2
+      }}
     >
-      <div>
+      <div className={cn('flex h-full', collapse && '')}>
         <div
           className={cn(
-            'h2-bold mb-1 px-3 mt-1 hidden max-lg/2:block max-lg/2:flex-center',
-            {
-              block: !collapse,
-            },
+            'overflow-y-scroll px-2 w-60 max-lg/2:w-20 custom-scrollbar-none h-full animate-fade-right',
+            collapse ? 'w-20' : ''
           )}
         >
-          <Image
-            className='size-6'
-            src='/assets/images/logo-icon.png'
-            width={40}
-            height={40}
-            alt='logo'
-          />
-        </div>
-        <div className={cn('h2-bold mb-1 px-3 max-lg/2:hidden')}>Instello</div>
-
-        {MenuItem.map((item, index) => (
-          <Link
-            href={item.link}
-            key={index}
-            onClick={() => {
-              if (item.onclick) item.onclick();
-            }}
+          <div
             className={cn(
-              'flex-start group mb-3 cursor-pointer rounded-lg px-3 py-2.5 hover:bg-hover-1 max-lg/2:flex-center',
+              'h2-bold px-3 mt-3 mb-3 hidden max-lg/2:block max-lg/2:flex-center',
+              collapse && 'block flex-center'
             )}
           >
-            <div className={cn('text-2xl mr-3 max-lg/2:mr-0 text-text-1')}>
-              {item.icon}
-            </div>
-            <div
-              className={cn(
-                'text-text-2 group-hover:text-text-1 max-lg/2:hidden',
-              )}
+            <Image
+              className='size-6'
+              src='/assets/images/logo-icon.png'
+              width={40}
+              height={40}
+              alt='logo'
+            />
+          </div>
+          <div
+            className={cn(
+              'h2-bold mb-1 px-3 max-lg/2:hidden',
+              collapse && 'hidden'
+            )}
+          >
+            Devhub
+          </div>
+
+          {MenuItem.map((item, index) => (
+            <Link
+              href={item.link}
+              key={index}
+              onClick={() => {
+                if (item.onclick) item.onclick();
+              }}
             >
-              {item.title}
+              <div
+                className={cn(
+                  'group mb-3 cursor-pointer rounded-lg px-3 py-2.5 hover:bg-hover-1 max-lg/2:flex-center',
+                  collapse ? 'flex-center' : 'flex-start'
+                )}
+              >
+                <div
+                  className={cn(
+                    'text-2xl mr-3 max-lg/2:mr-0 text-text-1',
+                    collapse && 'mr-0'
+                  )}
+                >
+                  {item.icon}
+                </div>
+                <div
+                  className={cn(
+                    'text-text-2 group-hover:text-text-1 max-lg/2:hidden',
+                    collapse && 'hidden'
+                  )}
+                >
+                  {item.title}
+                </div>
+              </div>
+            </Link>
+          ))}
+
+          {session ? (
+            <div className='mt-5'>
+              <HoverCard openDelay={100} onOpenChange={setOpen}>
+                <HoverCardTrigger
+                  className={cn(
+                    'max-lg/2:flex-center mb-2 cursor-default px-3',
+                    collapse ? 'flex-center' : 'flex-start'
+                  )}
+                  onMouseEnter={() => {
+                    CloseAllComponent();
+                    setCollapse(false);
+                  }}
+                >
+                  <Avatar
+                    className={cn(
+                      'size-7 me-3 max-lg/2:me-0',
+                      collapse && 'me-0'
+                    )}
+                    src={getImageURL(session?.user.image!)}
+                  />
+                  <span
+                    className={cn(
+                      'base-bold me-7 text-text-2 max-lg/2:hidden',
+                      collapse && 'hidden'
+                    )}
+                  >
+                    {session?.user.name || 'John Doe'}
+                  </span>
+                  <MdArrowForwardIos
+                    className={cn(
+                      'transition duration-200 max-lg/2:hidden',
+                      collapse && 'hidden',
+                      open && '-rotate-90'
+                    )}
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent className='mb-2 overflow-hidden rounded-lg border border-border-1 bg-foreground-1 py-3 text-text-1'>
+                  <AccountDetail />
+                </HoverCardContent>
+              </HoverCard>
             </div>
-          </Link>
-        ))}
-      </div>
-      {session ? (
-        <div className='mt-5'>
-          <HoverCard openDelay={100} onOpenChange={setOpen}>
-            <HoverCardTrigger className='flex-center lg:flex-start mb-2 cursor-default px-3'>
-              <Avatar
+          ) : (
+            <div className='flex-center lg:flex-start mb-2 cursor-default px-3'>
+              <Skeleton
                 className='size-7 lg:me-3'
-                src={getImageURL(session?.user.image!)}
+                variant='circular'
+                width={40}
+                height={40}
               />
-              <span className='base-bold me-7 text-text-2 max-lg/2:hidden'>
-                {session?.user.name || 'John Doe'}
-              </span>
+              <Skeleton
+                className='base-bold me-7 hidden lg:block'
+                variant='text'
+                width={100}
+                height={30}
+              />
               <MdArrowForwardIos
                 className={cn(
-                  'transition duration-200 max-lg/2:hidden',
-                  open && '-rotate-90',
+                  'hidden transition duration-200 lg:block',
+                  open && '-rotate-90'
                 )}
               />
-            </HoverCardTrigger>
-            <HoverCardContent className='mb-2 overflow-hidden rounded-lg border border-border-1 bg-foreground-1 py-3 text-text-1'>
-              <AccountDetail />
-            </HoverCardContent>
-          </HoverCard>
+            </div>
+          )}
         </div>
-      ) : (
-        <div className='flex-center lg:flex-start mb-2 cursor-default px-3'>
-          <Skeleton
-            className='size-7 lg:me-3'
-            variant='circular'
-            width={40}
-            height={40}
-          />
-          <Skeleton
-            className='base-bold me-7 hidden lg:block'
-            variant='text'
-            width={100}
-            height={30}
-          />
-          <MdArrowForwardIos
-            className={cn(
-              'hidden transition duration-200 lg:block',
-              open && '-rotate-90',
-            )}
-          />
+        <div
+          className={cn(
+            'search w-[380px] bg-foreground-1 overflow-y-scroll custom-scrollbar-fg py-6 h-full duration-300',
+            openSearch
+              ? 'animate-in slide-in-from-left border-s border-border-1'
+              : 'w-0 border-none'
+          )}
+        >
+          <Search />
         </div>
-      )}
+        <div
+          className={cn(
+            'search w-[380px] bg-foreground-1 overflow-y-scroll custom-scrollbar-fg py-6 h-full duration-300',
+            openNotification
+              ? 'animate-in slide-in-from-left border-s border-border-1'
+              : 'w-0 border-none'
+          )}
+        >
+          <Notification />
+        </div>
+      </div>
     </nav>
   );
 }
