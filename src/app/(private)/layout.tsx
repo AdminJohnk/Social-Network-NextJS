efce8fb8fa55988ee60dc43edf4e501695c14f01
ZLoganZ <ln26805@gmail.com>
2024-03-09 17:13:31 +0700

    Remove unused code and commented out SideBar component

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -4,7 +4,6 @@ import SideBar from '@/components/SideBar';
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <SideBar /> */}
       <Header />
       <SideBar />
       <div className='h-fit min-h-dvh'>{children}</div>
