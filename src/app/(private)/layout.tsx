76ad59bfb23bb6550870a2e88c742825b2e1ff40
Mr-LeeB <quangbaokh9a2@gmail.com>
2024-03-09 17:09:02 +0700

    Update layout and sidebar components

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,7 +3,7 @@ import SideBar from '@/components/SideBar/Sidebar';
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SideBar />
+      {/* <SideBar /> */}
       <div className='h-fit min-h-dvh'>{children}</div>
     </>
   );
--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -19,7 +19,7 @@ import Image from 'next/image';
 import Search from '../Search/Search';
 import Notification from '../Notification/Notification';
 
-export interface ISideBarProps {}
+export interface ISideBarProps { }
 
 export default function SideBar(props: ISideBarProps) {
   const [open, setOpen] = React.useState(false);
@@ -218,7 +218,7 @@ export default function SideBar(props: ISideBarProps) {
                         <path
                           fill-rule='evenodd'
                           d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                          clip-rule='evenodd'></path>
+                          clipRule='evenodd'></path>
                       </svg>
                       <span id='show__more'> See More </span>
                       <span className='!hidden' id='show__more'>
@@ -277,16 +277,16 @@ export default function SideBar(props: ISideBarProps) {
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
                             viewBox='0 0 24 24'
-                            stroke-width='1.5'
+                            strokeWidth='1.5'
                             stroke='currentColor'
                             className='w-4 h-4'>
                             <path
-                              stroke-linecap='round'
-                              stroke-linejoin='round'
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
                               d='M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z'></path>
                             <path
-                              stroke-linecap='round'
-                              stroke-linejoin='round'
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
                               d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'></path>
                           </svg>
                           <span> Setting </span>
@@ -298,12 +298,12 @@ export default function SideBar(props: ISideBarProps) {
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
                             viewBox='0 0 24 24'
-                            stroke-width='1.5'
+                            strokeWidth='1.5'
                             stroke='currentColor'
                             className='w-4 h-4'>
                             <path
-                              stroke-linecap='round'
-                              stroke-linejoin='round'
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
                               d='M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z'></path>
                           </svg>
                           <span> Upgrade </span>
@@ -315,12 +315,12 @@ export default function SideBar(props: ISideBarProps) {
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
                             viewBox='0 0 24 24'
-                            stroke-width='1.5'
+                            strokeWidth='1.5'
                             stroke='currentColor'
                             className='w-4 h-4'>
                             <path
-                              stroke-linecap='round'
-                              stroke-linejoin='round'
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
                               d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9'></path>
                           </svg>
                           <span> Authentication </span>
@@ -332,12 +332,12 @@ export default function SideBar(props: ISideBarProps) {
                             xmlns='http://www.w3.org/2000/svg'
                             fill='none'
                             viewBox='0 0 24 24'
-                            stroke-width='1.5'
+                            strokeWidth='1.5'
                             stroke='currentColor'
                             className='w-4 h-4'>
                             <path
-                              stroke-linecap='round'
-                              stroke-linejoin='round'
+                              strokeLinecap='round'
+                              strokeLinejoin='round'
                               d='M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z'></path>
                           </svg>
                           <span> Development </span>
