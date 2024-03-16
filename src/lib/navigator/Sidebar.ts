98cc058818e16d6200b78f1e5fe5f4e54cc3e36c
ZLoganZ <ln26805@gmail.com>
2024-03-16 17:04:08 +0700

    Refactor code for improved performance and readability

---
+++ src/lib/navigator/Sidebar.ts
@@ -0,0 +1,62 @@
+export const Sidebar: Array<{ href: string; image: string; label: string; showMore?: boolean }> = [
+  {
+    href: '/',
+    image: '/images/home/home.png',
+    label: 'Home'
+  },
+  {
+    href: '/messages',
+    image: '/images/home/message.png',
+    label: 'Messages'
+  },
+  {
+    href: '/events',
+    image: '/images/home/event.png',
+    label: 'Events'
+  },
+  {
+    href: '/community/123',
+    image: '/images/home/page.png',
+    label: 'Pages'
+  },
+  {
+    href: '/community',
+    image: '/images/home/group.png',
+    label: 'Groups'
+  },
+  {
+    href: '/blogs',
+    image: '/images/home/blog.png',
+    label: 'Blog'
+  },
+  {
+    href: '',
+    image: '/images/home/game.png',
+    label: 'Games',
+    showMore: true
+  },
+  {
+    href: '',
+    image: '/images/home/fund.png',
+    label: 'Fundraiser',
+    showMore: true
+  },
+  {
+    href: '',
+    image: '/images/home/blog-2.png',
+    label: 'Blog 2',
+    showMore: true
+  },
+  {
+    href: '',
+    image: '/images/home/event-2.png',
+    label: 'Event 2',
+    showMore: true
+  },
+  {
+    href: '',
+    image: '/images/home/group-2.png',
+    label: 'Group 2',
+    showMore: true
+  }
+];
