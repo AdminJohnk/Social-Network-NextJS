d2631e4ef16b6462e8ef18a5f0f3a8aa595b0972
Mr-LeeB <quangbaokh9a2@gmail.com>
2024-03-18 12:25:07 +0700

    Update function to handle edge cases

---
+++ src/components/pages/Profile/GroupsYouManage.tsx
@@ -0,0 +1,117 @@
+import { Link } from '@/navigation';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import * as React from 'react';
+
+export interface IGroupsYouManageProps {
+}
+
+export default function GroupsYouManage(props: IGroupsYouManageProps) {
+    const t = useTranslations();
+
+    return (
+        <div className='bg-foreground-1 rounded-xl shadow p-5 px-6 border1'>
+            <div className='flex items-baseline justify-between text-text-1'>
+                <h3 className='font-bold text-base'> {t('Suggested Manage')} </h3>
+                <Link href='#' className='text-sm text-blue-500'>
+                    {t('See all')}
+                </Link>
+            </div>
+
+            <div className='mt-5'>
+                <div className='flex items-center space-x-3 my-3'>
+                    <Link href='#'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src='/images/avatars/avatar-2.jpg'
+                            alt=''
+                            className='h-10 w-10 rounded-full'
+                        />
+                    </Link>
+                    <div className='flex-1'>
+                        <Link href='#'>
+                            <h4 className='text-sm leading-5 font-bold text-text-1'> John Michael</h4>
+                        </Link>
+                        <div className='mt-0.5 text-xs leading-4 text-text-2'>
+                            {t('Updated xx day ago', { count: 6 })}
+                        </div>
+                    </div>
+                    <button className='button bg-foreground-2 hover:bg-hover-2 dark:text-white'>
+                        {t('Like')}
+                    </button>
+                </div>
+                <div className='flex items-center space-x-3 my-3'>
+                    <Link href='#'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src='/images/avatars/avatar-4.jpg'
+                            alt=''
+                            className='h-10 w-10 rounded-full'
+                        />
+                    </Link>
+                    <div className='flex-1'>
+                        <Link href='#'>
+                            <h4 className='text-sm leading-5 font-bold text-text-1'> Martin Gray</h4>
+                        </Link>
+                        <div className='mt-0.5 text-xs leading-4 text-text-2'>
+                            {t('Updated xx month ago', { count: 2 })}
+                        </div>
+                    </div>
+                    <button className='button bg-foreground-2 hover:bg-hover-2 dark:text-white'>
+                        {t('Like')}
+                    </button>
+                </div>
+                <div className='flex items-center space-x-3 my-3'>
+                    <Link href='#'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src='/images/avatars/avatar-3.jpg'
+                            alt=''
+                            className='h-10 w-10 rounded-full'
+                        />
+                    </Link>
+                    <div className='flex-1'>
+                        <Link href='#'>
+                            <h4 className='text-sm leading-5 font-bold text-text-1'> Monroe Parker</h4>
+                        </Link>
+                        <div className='mt-0.5 text-xs leading-4 text-text-2'>
+                            {t('Updated xx week ago', { count: 1 })}
+                        </div>
+                    </div>
+                    <button className='button bg-foreground-2 hover:bg-hover-2 dark:text-white'>
+                        {t('Like')}
+                    </button>
+                </div>
+                <div className='flex items-center space-x-3 my-3'>
+                    <Link href='#'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src='/images/avatars/avatar-1.jpg'
+                            alt=''
+                            className='h-10 w-10 rounded-full'
+                        />
+                    </Link>
+                    <div className='flex-1'>
+                        <Link href='#'>
+                            <h4 className='text-sm leading-5 font-bold text-text-1'> Jesse Steeve</h4>
+                        </Link>
+                        <div className='mt-0.5 text-xs leading-4 text-text-2'>
+                            {t('Updated xx day ago', { count: 2 })}
+                        </div>
+                    </div>
+                    <button className='button bg-foreground-2 hover:bg-hover-2 dark:text-white'>
+                        {t('Like')}
+                    </button>
+                </div>
+            </div>
+
+            <button className='bg-foreground-2 hover:bg-hover-2 w-full text-text-1 py-1.5 font-medium px-3.5 rounded-md text-sm mt-2'>
+                {t('See all')}
+            </button>
+        </div>
+    );
+}
