7c068ba5aa0a25092697969ef58adb5ab49a80dc
ZLoganZ <ln26805@gmail.com>
2024-04-06 12:10:36 +0700

    Refactor middleware configuration and authentication logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,7 @@ import { withIntl } from '@/middlewares/withIntl';
 import { withAuthentication } from '@/middlewares/withAuthentication';
 
 export default stackMiddlewares([withAuthentication, withIntl]);
-// export default function middleware() {}
 
 export const config = {
-  matcher: [
-    '/',
-    '/(en|vi|cn)/:path*',
-    '/login',
-    '/register',
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    '/([\\w-]+)?/(.+)'
-  ]
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/([\\w-]+)?/(.+)']
 };
--- src/middlewares/withIntl.ts
+++ src/middlewares/withIntl.ts
@@ -1,12 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
 
 import { localePrefix } from '@/navigation';
+import { locales } from '@/i18n';
 import { MiddlewareFactory } from './types';
 
-export const withIntl: MiddlewareFactory = (next) => {
-  return createMiddleware({
-    locales: ['en', 'vi'],
+export const withIntl: MiddlewareFactory = (next) =>
+  createMiddleware({
+    locales,
     defaultLocale: 'en',
     localePrefix
   });
-};
