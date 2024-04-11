ac8b96c96f84ecf6384ffe9f6ec25bbcaf0ea93f
AdminJohnk <truongtantai07042002@gmail.com>
2024-04-11 14:09:26 +0700

    Remove ToastContainer from SocialTab component

--- src/components/pages/EditProfile/SocialTab.tsx
+++ src/components/pages/EditProfile/SocialTab.tsx
@@ -218,7 +218,7 @@ export default function SocialTab(props: ISocialTabProps) {
           </Button>
         </div>
       </div>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
     </form>
   );
 }
--- src/components/ui/toast.ts
+++ src/components/ui/toast.ts
@@ -1,7 +1,9 @@
 import { Bounce, toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { theme } from 'flowbite-react';
 
 export const showSuccessToast = (mes: string) => {
+
   toast.success(mes, {
     position: 'top-right',
     autoClose: 3000,
@@ -12,7 +14,6 @@ export const showSuccessToast = (mes: string) => {
     progress: undefined,
     theme: 'dark',
     transition: Bounce,
-    className: 'top-12'
   });
 };
 
@@ -27,7 +28,6 @@ export const showErrorToast = (mes: string) => {
     progress: undefined,
     theme: 'dark',
     transition: Bounce,
-    className: 'top-12'
   });
 };
 
