58824a7464d12305305f1a3ec0674c7d24804b87
ZLoganZ <ln26805@gmail.com>
2024-04-14 18:27:34 +0700

    Refactor import of Popover in NewPostShare.tsx

--- src/components/shared/NewPostShare/NewPostShare.tsx
+++ src/components/shared/NewPostShare/NewPostShare.tsx
@@ -15,23 +15,18 @@ import Textarea from '@/components/ui/textarea';
 import Picker from '@emoji-mart/react';
 import { useThemeMode } from 'flowbite-react';
 import { IoHappyOutline } from 'react-icons/io5';
-import Popover from '@/components/ui/popover';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 export interface INewPostShareProps {
   handleClose: () => void;
   post: IPost;
 }
 
-export default function NewPostShare({
-  handleClose,
-  post
-}: INewPostShareProps) {
+export default function NewPostShare({ handleClose, post }: INewPostShareProps) {
   const t = useTranslations();
   const { mode } = useThemeMode();
   const { data: session } = useSession();
-  const { currentUserInfo, isLoadingCurrentUserInfo } = useCurrentUserInfo(
-    session?.id || ''
-  );
+  const { currentUserInfo, isLoadingCurrentUserInfo } = useCurrentUserInfo(session?.id || '');
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [privacy, setPrivacy] = useState('public');
@@ -46,32 +41,14 @@ export default function NewPostShare({
       {isLoadingCurrentUserInfo ? (
         <div className='flex-between'>
           <div className='flex-start gap-3'>
-            <Skeleton
-              className='bg-foreground-2'
-              variant='circular'
-              width={40}
-              height={40}
-            />
+            <Skeleton className='bg-foreground-2' variant='circular' width={40} height={40} />
             <div className='flex flex-col'>
-              <Skeleton
-                className='bg-foreground-2 w-36'
-                variant='text'
-                sx={{ fontSize: '1.5rem' }}
-              />
-              <Skeleton
-                className='bg-foreground-2 w-36'
-                variant='text'
-                sx={{ fontSize: '1rem' }}
-              />
+              <Skeleton className='bg-foreground-2 w-36' variant='text' sx={{ fontSize: '1.5rem' }} />
+              <Skeleton className='bg-foreground-2 w-36' variant='text' sx={{ fontSize: '1rem' }} />
             </div>
           </div>
           <div>
-            <Skeleton
-              className='bg-foreground-2'
-              variant='circular'
-              width={25}
-              height={25}
-            />
+            <Skeleton className='bg-foreground-2' variant='circular' width={25} height={25} />
           </div>
         </div>
       ) : (
@@ -81,10 +58,7 @@ export default function NewPostShare({
               <Avatar src={getImageURL(currentUserInfo?.user_image)} />
             </Link>
             <div className='flex flex-col ms-3'>
-              <Link
-                href={`/profile/${currentUserInfo?._id}`}
-                className='base-bold'
-              >
+              <Link href={`/profile/${currentUserInfo?._id}`} className='base-bold'>
                 {currentUserInfo?.name}
               </Link>
             </div>
@@ -94,29 +68,46 @@ export default function NewPostShare({
               className='w-full mt-3'
               placeholder={t('What do you have in mind?')}
               value={text}
-              onChange={event => setText(event.target.value)}
+              onChange={(event) => setText(event.target.value)}
               maxRows={7}
             />
-            <Popover
+            {/* <Popover
               open={false}
               mainContent={<IoHappyOutline className='text-2xl flex' />}
               hoverContent={
                 <Picker
                   data={async () => {
-                    const response = await fetch(
-                      'https://cdn.jsdelivr.net/npm/@emoji-mart/data'
-                    );
+                    const response = await fetch('https://cdn.jsdelivr.net/npm/@emoji-mart/data');
 
                     return response.json();
                   }}
                   onEmojiSelect={(emoji: IEmoji) => {
                     console.log(emoji.native);
-                    addEmoji(emoji.native)
+                    addEmoji(emoji.native);
                   }}
                   theme={mode}
                 />
               }
-            />
+            /> */}
+            <Popover>
+              <PopoverTrigger>
+                <IoHappyOutline className='text-2xl flex' />
+              </PopoverTrigger>
+              <PopoverContent>
+                <Picker
+                  data={async () => {
+                    const response = await fetch('https://cdn.jsdelivr.net/npm/@emoji-mart/data');
+
+                    return response.json();
+                  }}
+                  onEmojiSelect={(emoji: IEmoji) => {
+                    console.log(emoji.native);
+                    addEmoji(emoji.native);
+                  }}
+                  theme={mode}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       )}
@@ -129,11 +120,8 @@ export default function NewPostShare({
           type='submit'
           className='button lg:px-6 text-white max-md:flex-1'
           // disabled={!isChanged || isLoading}
-          onClick={onSubmit}
-        >
-          {isLoading && (
-            <CircularProgress size={20} className='text-text-1 mr-2' />
-          )}
+          onClick={onSubmit}>
+          {isLoading && <CircularProgress size={20} className='text-text-1 mr-2' />}
           {t('Share')} <span className='ripple-overlay'></span>
         </Button>
       </div>
