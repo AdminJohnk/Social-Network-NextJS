32adbb0cf183407fd5369ede7918973f9d597af3
ZLoganZ <ln26805@gmail.com>
2024-04-25 17:05:57 +0700

    Refactor ContextMenuConversationBox component and remove commented code

--- src/components/pages/Chat/ContextMenuConversationBox.tsx
+++ src/components/pages/Chat/ContextMenuConversationBox.tsx
@@ -49,69 +49,6 @@ export default function ContextMenuConversationBox({
     return conversation.members.find((member) => member._id !== currentUserInfo._id);
   }, [currentUserInfo, conversation?.members]);
 
-  // const items: MenuProps['items'] = [
-  //   {
-  //     label: isSeen ? 'Undo reading' : 'Mark as read',
-  //     style: { display: isLastMessageFromCurrentUser ? 'none' : 'block' },
-  //     key: '1',
-  //     icon: <FontAwesomeIcon icon={isSeen ? faReSquareCheck : faSquareCheck} />,
-  //     onClick: () => {
-  //       const emitType = isSeen ? Socket.UNSEEN_MSG : Socket.SEEN_MSG;
-  //       chatSocket.emit(emitType, { conversationID: conversation._id, userID: currentUserInfo._id });
-  //     }
-  //   },
-  //   {
-  //     label: 'Mute notifications',
-  //     key: '2',
-  //     icon: <FontAwesomeIcon icon={faBellSlash} />
-  //   },
-  //   {
-  //     label: 'View profile',
-  //     key: '4',
-  //     icon: <FontAwesomeIcon icon={faUser} />,
-  //     onClick: () => navigate(`/user/${otherUser._id}`),
-  //     style: { display: isGroup ? 'none' : 'block' }
-  //   },
-  //   { type: 'divider' },
-  //   {
-  //     label: 'Audio call',
-  //     key: '5',
-  //     icon: <FontAwesomeIcon icon={faPhone} />,
-  //     onClick: () => audioCall(conversation._id)
-  //   },
-  //   {
-  //     label: 'Video chat',
-  //     key: '6',
-  //     icon: <FontAwesomeIcon icon={faVideoCamera} />,
-  //     onClick: () => videoChat(conversation._id)
-  //   },
-  //   { type: 'divider' },
-  //   {
-  //     label: isGroup ? 'Leave group' : 'Delete chat',
-  //     danger: true,
-  //     key: '3',
-  //     onClick: () => {
-  //       mutateLeaveGroup(conversation._id);
-  //       const message = {
-  //         _id: uuidv4().replace(/-/g, ''),
-  //         conversation_id: conversation._id,
-  //         sender: {
-  //           _id: currentUserInfo._id,
-  //           user_image: currentUserInfo.user_image,
-  //           name: currentUserInfo.name
-  //         },
-  //         isSending: true,
-  //         type: 'notification',
-  //         content: 'left the group',
-  //
-  //       };
-
-  //       mutateSendMessage(message as unknown as IMessage);
-  //       chatSocket.emit(Socket.PRIVATE_MSG, { conversationID: conversation._id, message });
-  //     },
-  //     icon: <FontAwesomeIcon icon={isGroup ? faRightFromBracket : faTrash} />
-  //   }
-  // ];
   return (
     <ContextMenu>
       <ContextMenuTrigger>{children}</ContextMenuTrigger>
