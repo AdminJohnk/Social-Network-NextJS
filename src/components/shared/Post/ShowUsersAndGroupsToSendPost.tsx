0a04f865cbd277c60d52b53090ed5f8906099a74
Mr-LeeB <quangbaokh9a2@gmail.com>
2024-05-06 11:50:47 +0700

    Refactor types/index.ts to add 'post' as a valid message type

--- src/components/shared/Post/ShowUsersAndGroupsToSendPost.tsx
+++ src/components/shared/Post/ShowUsersAndGroupsToSendPost.tsx
@@ -7,17 +7,22 @@ import { IoSearchOutline } from 'react-icons/io5';
 import { MdPublic } from 'react-icons/md';
 import { FaUserFriends } from 'react-icons/fa';
 import { IoMdLock } from 'react-icons/io';
+import { v4 as uuidv4 } from 'uuid';
+
+import Image from 'next/image';
 
 import { useConversationsData, useCurrentUserInfo, usePostData } from '@/hooks/query';
 import { Avatar, CircularProgress } from '@mui/material';
 import AvatarMessage from '@/components/pages/Chat/Avatar/AvatarMessage';
-import { IPost, IUserInfo } from '@/types';
+import { IMessage, IPost, IUserInfo } from '@/types';
 import { Button } from '@/components/ui/button';
 import { cn, getImageURL } from '@/lib/utils';
 import { Link } from '@/navigation';
-import Image from 'next/image';
 import ShowContent from '../ShowContent/ShowContent';
 import AvatarGroup from '@/components/pages/Chat/Avatar/AvatarGroup';
+import { Socket } from '@/lib/utils/constants/SettingSystem';
+import { useSendMessage } from '@/hooks/mutation';
+import { useSocketStore } from '@/store/socket';
 
 export interface IShowUsersAndGroupsToSendPostProps {
   post_id: string;
@@ -38,21 +43,9 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
   const [checkList, setCheckList] = useState<Record<string, boolean>>({});
   const [checkedUsers, setCheckedUsers] = useState<IUserInfo[]>([]);
 
-  const HandleOnClick = (userID: string) => {
-    setCheckList({ ...checkList, [userID]: !checkList[userID] });
-    if (checkList[userID]) {
-      setCheckedUsers(checkedUsers.filter((user) => user._id !== userID));
-    } else {
-      setCheckedUsers([...checkedUsers, members.find((user) => user._id === userID)!]);
-    }
-  };
-
-  const handleUncheck = (userID: string) => {
-    setCheckList({ ...checkList, [userID]: false });
-    setCheckedUsers(checkedUsers.filter((user) => user._id !== userID));
-  };
-
   const { conversations, isLoadingConversations } = useConversationsData();
+
+
 
   const groups = useMemo(() => {
     return conversations.filter((conversation) => conversation.type === 'group');
@@ -112,8 +105,43 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
       })
     );
   }, []);
+
+  const [id, setId] = useState(uuidv4().replace(/-/g, ''));
+  const { mutateSendMessage } = useSendMessage();
+  const [messageContent, setMessage] = useState('');
+
+  const { chatSocket } = useSocketStore();
+
+  const handleSubmit = async (content: string, ID: string) => {
+    if (!ID) return;
+
+    setMessage('');
+
+    if (content.trim() !== '' || content.trim().length !== 0) {
+      const message = {
+        _id: id,
+        conversation_id: ID,
+        sender: {
+          _id: currentUserInfo._id,
+          user_image: currentUserInfo.user_image,
+          name: currentUserInfo.name
+        },
+        type: 'post',
+        isSending: true,
+        content: content,
+        post_id: post_id,
+        seen: [],
+        createdAt: new Date()
+      };
+
+      setId(uuidv4().replace(/-/g, ''));
+      mutateSendMessage(message as unknown as IMessage);
+      chatSocket.emit(Socket.PRIVATE_MSG, { conversationID: ID, message });
+      chatSocket.emit(Socket.STOP_TYPING, { conversationID: ID, userID: currentUserInfo._id, members });
+    }
+  };
   return (
-    <div className='w-[740px] overflow-y-scroll bg-foreground-1 custom-scrollbar-fg p-7 animate-fade-up rounded-lg'>
+    <div className='w-[740px] max-h-[780px] overflow-y-scroll bg-foreground-1 custom-scrollbar-fg p-7 animate-fade-up rounded-lg'>
       <div>
         <div className='flex flex-col w-full gap-5'>
           <div className='font-bold text-lg text-center'>{t('Share')}</div>
@@ -148,7 +176,7 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
                 </div>
               </div>
               <div className=''>
-                <div className='flex-between gap-2'>
+                <div className='flex-between items-start gap-2'>
                   <div className={cn(post!.post_attributes.images.length > 0 ? 'w-5/6' : 'w-full')}>
                     <ShowContent content={content.length > 250 ? content.slice(0, 250) + '...' : content} />
                   </div>
@@ -175,8 +203,7 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
               placeholder={t('Please write something for this content') + '...'}
               className='w-full !py-2 rounded-lg bg-foreground-1 border-none active:border-none border focus:ring-0'
               onChange={(e) => {
-                // setSearch(e.target.value);
-                // if (!isLoadingSearch) setIsLoadingSearch(true);
+                setMessage(e.target.value);
               }}
             />
           </div>
@@ -203,32 +230,39 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
               <>
                 <div className='font-bold text-lg text-left'>{t('Recent')}</div>
                 <div className='flex flex-col gap-5'>
-                  {conversations.slice(0, 5).map((conversation) => (
-                    <div
-                      className='conversation flex items-center justify-between'
-                      key={conversation._id}
-                    >
-                      <div className='info flex items-center'>
-                        <div className='avatar relative'>
-                          {conversation.type !== 'group' ?
-                            (
-                              <Avatar src={getImageURL(conversation.cover_image)} />
-                            ) :
-                            (
-                              <AvatarGroup
-                                key={conversation._id}
-                                users={conversation.members}
-                                image={conversation.image} />
-                            )}
+                  {conversations.slice(0, 5).map((conversation) => {
+                    const isGroup = conversation.type === 'group';
+                    const otherUser = conversation.members.find((member) => member._id !== currentUserInfo._id);
+
+                    return (
+                      <div
+                        className='conversation flex items-center justify-between'
+                        key={conversation._id}
+                      >
+                        <div className='info flex items-center'>
+                          <div className='avatar relative'>
+                            {!isGroup ?
+                              (
+                                <AvatarMessage key={otherUser!._id} user={otherUser!} />
+                              ) :
+                              (
+                                <AvatarGroup
+                                  key={conversation._id}
+                                  users={conversation.members}
+                                  image={conversation.image} />
+                              )}
+                          </div>
+                          <div className='name text-center ml-2 font-bold'>{conversation.name || otherUser!.name}</div>
                         </div>
-                        <div className='name text-center ml-2 font-bold'>{conversation.name}</div>
+                        <Button className='base-bold !bg-foreground-2 hover:bg-hover-2 duration-300 text-text-2 px-4 py-1 rounded-2xl items-end mr-1'
+                          onClick={() => {
+                            handleSubmit(messageContent, conversation._id);
+                          }}>
+                          {t('Send')}
+                        </Button>
                       </div>
-                      <Button className='base-bold !bg-foreground-2 hover:bg-hover-2 duration-300 text-text-2 px-4 py-1 rounded-2xl items-end mr-1'
-                        onClick={() => { }}>
-                        {t('Send')}
-                      </Button>
-                    </div>
-                  ))}
+                    )
+                  })}
                 </div>
               </>
             )}
@@ -239,7 +273,7 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
               </div>
             ) : groups.length > 0 && (
               <>
-                <div className='font-bold text-lg text-left mt-3'>{t('Groups')}</div>
+                <div className='font-bold text-lg text-left mt-5'>{t('Groups')}</div>
                 <div className='flex flex-col gap-5'>
                   {groups.map((group) => (
                     <div
@@ -257,7 +291,9 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
                         <div className='name text-center ml-2 font-bold'>{group.name}</div>
                       </div>
                       <Button className='base-bold !bg-foreground-2 hover:bg-hover-2 duration-300 text-text-2 px-4 py-1 rounded-2xl items-end mr-1'
-                        onClick={() => { }}>
+                        onClick={() => {
+                          handleSubmit(messageContent, group._id);
+                        }}>
                         {t('Send')}
                       </Button>
                     </div>
@@ -272,7 +308,7 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
               </div>
             ) : members.length > 0 && (
               <>
-                <div className='font-bold text-lg text-left mt-3'>{t('Contacts')}</div>
+                <div className='font-bold text-lg text-left mt-5'>{t('Contacts')}</div>
                 <div className='flex flex-col gap-5'>
                   {members.map((user) => (
                     <div
@@ -286,7 +322,9 @@ export default function ShowUsersAndGroupsToSendPost({ post_id, content }: IShow
                         <div className='name text-center ml-2 font-bold'>{user.name}</div>
                       </div>
                       <Button className='base-bold !bg-foreground-2 hover:bg-hover-2 duration-300 text-text-2 px-4 py-1 rounded-2xl items-end mr-1'
-                        onClick={() => { }}>
+                        onClick={() => {
+                          handleSubmit(messageContent, user._id);
+                        }}>
                         {t('Send')}
                       </Button>
                     </div>
