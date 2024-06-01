ed32a9249289b8a025b8bc271b2ee02a1abe402f
AdminJohnk <truongtantai07042002@gmail.com>
2024-06-01 18:03:21 +0700

    Fix error handling in QuestionDetail component

--- src/app/[locale]/(private)/(withSidebar)/questions/[id]/page.tsx
+++ src/app/[locale]/(private)/(withSidebar)/questions/[id]/page.tsx
@@ -13,6 +13,7 @@ import Modal from '@/components/shared/Modal';
 import CreateEditQuestion from '@/components/pages/Question/CreateEditQuestion';
 import { useGetQuestionByID } from '@/hooks/query';
 import { useViewQuestion } from '@/hooks/mutation';
+import { notFound } from 'next/navigation';
 
 export interface IQuestionDetailProps {
   params: {
@@ -29,7 +30,8 @@ export default function QuestionDetail({
 
   const [sortBy, setSortBy] = useState('scoredesc');
 
-  const { question, isLoadingQuestion } = useGetQuestionByID(id);
+  const { question, isLoadingQuestion, isErrorQuestion } =
+    useGetQuestionByID(id);
   const { mutateViewQuestion } = useViewQuestion();
 
   useEffect(() => {
@@ -52,7 +54,7 @@ export default function QuestionDetail({
 
   // Modal
   const [openCreateQuestion, setOpenCreateQuestion] = useState(false);
-
+  if (isErrorQuestion) notFound();
   return (
     <>
       {isLoadingQuestion ? (
